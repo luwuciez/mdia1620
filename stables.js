a2314b604c509c9d19598f8734a3d0633e9b5148
Lucie Zhong <luciez@Lucies-MacBook-Pro.local>
2024-09-19 15:35:43 -0700

    Created stables.js

---
+++ stables.js
@@ -0,0 +1,16 @@
+console.log("I have a horse.");
+
+let horseName = "David";
+console.log("The name of my horse is " + horseName + ".");
+console.log("Sometimes " + horseName + " is a little dumb,"); // He's still a good boy though :)
+
+horseName = "Dummy";
+console.log("so I like to call him " + horseName + ".");
+
+let horseAge = 7;
+console.log(horseName + " is " + horseAge + " years old.");
+
+let isHorseInStable = true;
+if (isHorseInStable==true) {
+    console.log(horseName + " is in the stable right now.");
+}
