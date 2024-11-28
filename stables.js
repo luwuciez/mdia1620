0dcf6d5c2496f50a656a6719d36573a1e4fc5cde
Lucie Zhong <luciez@Lucies-MacBook-Pro.local>
2024-11-28 09:54:01 -0800

    added project requirements

--- stables.js
+++ stables.js
@@ -1,49 +1,128 @@
-let horseName = "David";
-let horseNickName = "Dummy";
-let horseAge = 7;
+// Welcome!
+//
+//                                                  #    #
+//                                             %%% ##   ##
+//                                          %%%%% ###%🎀###
+//                                         %%%%% ### %%% #
+//                                       %%%%%% ### %%% ###
+//                                        %%%% ## %% #######
+//                                       %%%%% # %% #@#####
+//                                     %%%%%% # % #########
+//                                    %%%%% ##### #########
+//                          ###        %% ####### #########
+//                 %%% ############    ########### ########
+//              %%%% ############################### #######
+//            %%%%% ################################## ######
+//          %%%%%% #################################### #C###
+//         %%%🌸%% #####################################  ###
+//         %%%%% #######################################
+//        %%%%%% ############### BEANS ##################
+//     % %%%%%%% ############### BEANS ##################
+//      %%%%%%%%% ############## BEANS ##################
+//     %%%%%%%%%% ########################################
+//  %%% %%%%%%%%   ###### ################################
+//    %%%%%%%%      ###### #################### ##########
+// % %%%🌸%%%%        ####### ########### ###### ##########
+//  %%%%%%%%%         #######  ########### ###### ########
+// %%%%%%%%%%          ##### ###  ######### ####### ######
+//  %%%%%%%%%%          #### ##               ####### ####
+//  %🌸%%%%%%%%%           ## #                  ##### ###
+//   %%  %% % %%         # ##                      ## ###
+//     %   %    %        # ###                      # ###
+//                        # ###                     ## ###
+//                        # ###                     ## ###
+//                        # ####                   #### ##
+//                       ### ###                  ##### ###
+//                      ####  ###                 ####   ##
+//                     #####   ###                 ##    ##
+//                    #####    ####                      ###
+//                     ##        ###                     ###
+//                                ####                     ##
+//                                 ####                    ###
+//                                                         ####
+//                                                          ##
+//
+// This is a template for the final project. You'll be setting up a stable and making
+// sure it runs smoothly.
+//
+// You can copy and paste this into your stables.js file, and then use it to guide you.
+// Please do not create a new file!
+// Feel free to keep the headings (e.g.: "Variables"), but please delete any boilerplate
+// comments. (Your own comments, where necessary, are acceptable.)
+//
+// NOTE: UNLESS SPECIFIED, VARIABLE NAMING IS UP TO YOU. THERE SHOULD BE NO HARD-CODED
+// NUMBERS OR STRINGS WHERE VARIBALES WOULD BE PREFERRED.
 
-const STABLE_MONTHLY_FEE = 50;
-const MONTHLY_INCOME = 120;
+//------------------------- Seting up shop -------------------------//
 
-let monthlyNetIncome = MONTHLY_INCOME - STABLE_MONTHLY_FEE;
-let spendingIntroduction = `I make $${MONTHLY_INCOME} each month, and spend $${STABLE_MONTHLY_FEE} to board ${horseNickName}. So My monthly net income is $${monthlyNetIncome}.`;
+// Initialize a variable called "horses" that will store the horses in your stable.
+// Create a variable and use it to store a message visitors to the stable.
+// Create a variable and use it to store the cost of a late payment fee.
+// Create a variable and use it to store the number of available stalls in the stable.
 
-console.log(spendingIntroduction);
+//------------------------- First day -------------------------//
 
-const DISCOUNT = 10;
-let threeMonthFee = STABLE_MONTHLY_FEE * 3;
-let savings = threeMonthFee * Number(DISCOUNT / 100);
-console.log(
-    `After getting a ${DISCOUNT}% discount, I can save $${savings} for a 3-month stay at the stable.`
-);
+// Using a constructor, add at least 3 horses to your stables.
+//
+// The horse should have the following properties:
+// - name, nickname, favorite treat (string)
+// - age, monthly rent (number)
+// - location (boolean inside/outside)
+// - two unique properties of your choice (use any primitive)
+// - a method that introduces your horse and references at least 3 object properties.
+// - a second, unique method of your choice
 
-// Lesson 6
-let myHorses = [
-    [horseName, horseNickName, horseAge, "brown", true],
-    ["Steve", "Blue", 3, "spotted", false],
-    ["Miku", "Diva", 9, "teal", true],
-];
+// Store the horses you've just created in your "horses" variable.
 
-// intro to horses
-// whether horses are inside or outside
-console.log(`I have ${myHorses.length} horses`);
+// Fancy! Another horse wants to be stabled! Create a variable that stores an object
+// literal of your new horse, and add it to your "horses" variable.
 
-myHorses.forEach((horse) => {
-    let horseIntroduction = "";
+// Initialize new property to your horses: a boolean that allows you to check if your horse
+// is hungry or not. Use dot notation.
 
-    horseIntroduction = horseIntroduction.concat(
-        `${horse[0]}'s nickname is ${horse[1]}, they're a ${horse[2]} year-old ${horse[3]} horse;`
-    );
+//------------------------- Stable roster -------------------------//
 
-    if (horse[4]) {
-        horseIntroduction = horseIntroduction.concat(
-            ` ${horse[1]} is inside the stable right now.`
-        );
-    } else {
-        horseIntroduction = horseIntroduction.concat(
-            ` ${horse[1]} is outside playing right now.`
-        );
-    }
+// We want to keep track of our horses and how they are doing. Paste the data structure
+// of our horse variable below so we can reference it. It is okay to comment it out.
 
-    console.log(horseIntroduction);
-});
+//---------------------- Growing business ----------------------//
+
+// We've added some horses! Re-assign the variable that contains the number of available stalls.
+// Use the math operator to determine how many stables you should have left, given the number of
+// horses you've added.
+
+// Express the following in code: If the variable that contains your remaining stalls
+// is less than 2, log out "We need to build more stalls", otherwise log out "We have
+// [number of stalls] available!"
+
+// Create a function that logs out how much will an individual horse will owe if rent is
+// paid late. Invoke the function.
+
+// Add loop that checks to find out if a certain horse likes a treat. If the horse does not like
+// the treat, log out their disatisfaction and check the next horse. Exit the loop when you find
+// the horse that likes the treat.
+
+// Create and invoke a function that returns (not logs) the nickname of a chosen horse. Log out
+// the return value outside of the function.
+
+//------------------------- Day to day operations -------------------------//
+
+// Create and invoke a function references one of your unique horse properties. It should
+// contain a conditional or a loop.
+
+// Morning! Create and invoke a function that moves all your horses outside. It should
+// log out statement indicating the horses have been moved to spend time in the sun.
+
+// Initialize a method on your horses that moves them inside if they are outside, and
+// vice versa.
+
+// Food! Create and invoke a function that feeds your horses. If the horse is outside, it
+// should call them in, and then all horses should be fed a treat. Log the activity of
+// the horses.
+
+// Bedtime! Update the function that lets your horses outside so that it also calls them
+// in if it is getting dark. Only the horses that moved should log out a message. Invoke the function.
+
+//------------------------- Bonus -------------------------//
+
+// Replace the ascii art at the top of the file with art representing your stable.
