8848a2429edc7bc3f10222a39f3e61a42f4a8a0b
Lucie Zhong <luciez@Lucies-MacBook-Pro.local>
2024-12-04 16:52:16 -0800

    finished stables

--- stables.js
+++ stables.js
@@ -1,128 +1,333 @@
-// Welcome!
+//                                              .=.
+//                                          .:==-.
+//                                        :====-        .:==:
+//                                      :======       .=====
+//                                    .=======.      ======.       .-==========-..
+//                                   :========      =======      .==================:
+//                                  :========:     -======-     :====================-.
+//                                 :=========.     =======:    :===============:
+//                                 ==========.    .=======-    =============.
+//                                 ==========:    .========   -==========-
+//                                .===========    .========.  ==========.
+//                          .+    .===========.    ========= .=========             :-=+======-:.      .=:
+//                          ==.    ============    :========-:=======-          .===============+++=. :==.        .=+:
+//                         ===-    ============-    =================         :=:....:=====.  ::==+++++++   .-=+==:
+//                        -====    .===========+-   :===============.            :==+=- .:===============🌺-===:
+//                        ==+==-    -===✨=========   ==============-          .=====. :+===============+==:
+//                       :======:    =+=========+=+.  ===+=======+=.        :+=++=. -==+===============++==:
+//                       -=+==+==:    =+=++===+=++===  ==+==+==+===       .+=+==- .+=============+Ⓞ=========
+//                       -=+===+=+:    ==+=+=======+++:.=+=====+=+=      :+=+=+: :==+==========++=+=+=====+=+
+//                       :+=++==+=+-    ====++=+++==+===-+=+++✨+++=     :+===+: -++++=++++=++=+=====+=+=+=+=+:
+//                        =++=+=+=+==.   =++=+===+=+=+=+=+===+=====     =+=++= :+=+=+====+=+=+=+. :=+=====++=+.
+//                        :+=++++++++==   =+=++++=+=+=+=+=+++=+++++-    ++==+. +++=++++++=+=++=:    .:==+=+====
+//                         :=+++=++++++++. :++=++++++++++++++++++++=   .+++== :*=+++++++++++++=          :+=+++:
+//                          .+++++=++++++=+=:=++++++++++++++++++=+++:   =+==- =+++++++++++++++=            =+++-
+//                            ==+++++++++++++++=+++++++++++++++++++++   =+++: +=+++++++++++++++-            +-
+//                              :+=++=+++++++++++++++++++++++++++++++=  .+++.:+++++++++++++++++*:
+//                                 =+=++++++++++++++++✨++++++++++++=+++  :++.:+++++++++++++++++++:
+//                            :.      :=++++++++++++++++++++++++++++++++  -+::++++++++++++++++++++=
+//                             :++=:      :-+**++++++++++++++++++++++++++. --:++++++++++++++++++++++
+//                               +++++++=-:..  :=+++++++++++++++++++++++++- .:+++++++++++++++++++++++:
+//                                 +++*++++++++++++++++++++++++++++++++*+++*. *++++++++++++++++++++*=+:
+//                                   -++++++++++++++++++++++++++++++++*++++.-+++++++++++++++++++++++*+:
+//                                      :+++++++++++++++++++++++++*+++++++: =++++++++++++++++++++++++++=
+//                                          .-+*++++*+*+++++++**+++++*=: :+++*+*+++++++++++++++++++++++=           :-=-:
+//                                                 :-==+**++++++=:.   :**+++++++++++++++++++++++++++*++:   .:=++*+*+++*++
+//                                                               :=+++*++*+*++++++++++++++++++++++++++++++*++*+++*+**:-**=
+//                            :+*+**+**=.          .:=+**+*****+++++*++*++++*+*+*+*+*+*+*+*+*++*+++*+*+*++++++++*=..=  -+*.
+//                         :=+*++*+**++*+*=    .=+**++*+*++++++*+*+++*++*+*++++++++++++++++++*++*+*++++**++*+*+::=**+. :++-
+//                       :+*+++*++++*++*++++ :*+++++*++++*+*+*++++*+++*++++*+*+*+*+*+*+*+*+*++*++++*+*++*+*+:      +*. :+*=
+//                     :*+++**+++*+*+.   := +++**+++*+*+*+++++*+*++*+*+*+*++++*+*+*+*+*+*+*+*++*+*++++++*:        =+*  :+++
+//                   -+**+*+++*+*++        *++*++*+++*+++*+*+++++*++++++++*+*++++++++++++++++*++++**+=.          -+*=  =*+*
+//                 =*+++*++*++++*=        +++*+*+++*+++*+++++*+*+++*+*+*+++++++++++++++++*+*++*++:              :*++   :=*+
+// :.          .:+*+*+*+++*+*+**=         ++*+*+*+*+*+*+*+*+*+*+*+*+*+*+**+*+*+******+++**++**=                .*+*=    :++
+//  =*+**+****++*+*++++*+*++*+*=          *+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+++++++++***++*+=                    .++   =*+*-
+//   -*+**+++**++*+*+*++*+*+++=           +++*+*+*+*+*+*+*+*+++++*+*+*+*+*+*+***+***+++=.                    ++**+.   :**=
+//     =+*++*+*+*+*+***++*++*=            =**+*+*++++++++*++**+**+++++******++===-:                          ++*=      ==
+//       =*+*+****+*+*+++*+*=              +**+*+*+*+*+*++*+++*.:===:                                        *-
+//          .==+==-..=*++**=               =*+*+*++++*++*+*+*+ =+*++=
+//           :..:-=*++**++:                 ++*++*+*+*+*+*+**:.***+*-
+//             =+*++**++=                   :****+*+*+*+*+*+= ++*+*+:
+//                 .:.                       ++++*+*+*+*+***.-*+***=
+//                                           -*+****+****+*.:***+++
+//                                           =+*+*+*+*+++*.-++*+**.
+//                                          .*+**+*+****= =****++
+//                                         :*++*+*****= -**+*+*:
+//                                       -*****+*++*: -*+*+*+-
+//                                    =***+**++++: :****+**=
+//                                  =*****+**=.     -*+**+=
+//                                  :+****=          .++*+*
+//                                   =**+=             :*+*=
+//                                    +**=              .++*=
+//                                    .++*                =**+
+//                                     =**-                -***:
+//                                      ***.                :**+=
+//                                      -**+.                :****+.
+//                                      .****-                =+*****=
+//                                       *+****:                  =***+-
+//                                          =***+:                 --====
+//                                          .**+++:
 //
-//                                                  #    #
-//                                             %%% ##   ##
-//                                          %%%%% ###%🎀###
-//                                         %%%%% ### %%% #
-//                                       %%%%%% ### %%% ###
-//                                        %%%% ## %% #######
-//                                       %%%%% # %% #@#####
-//                                     %%%%%% # % #########
-//                                    %%%%% ##### #########
-//                          ###        %% ####### #########
-//                 %%% ############    ########### ########
-//              %%%% ############################### #######
-//            %%%%% ################################## ######
-//          %%%%%% #################################### #C###
-//         %%%🌸%% #####################################  ###
-//         %%%%% #######################################
-//        %%%%%% ############### BEANS ##################
-//     % %%%%%%% ############### BEANS ##################
-//      %%%%%%%%% ############## BEANS ##################
-//     %%%%%%%%%% ########################################
-//  %%% %%%%%%%%   ###### ################################
-//    %%%%%%%%      ###### #################### ##########
-// % %%%🌸%%%%        ####### ########### ###### ##########
-//  %%%%%%%%%         #######  ########### ###### ########
-// %%%%%%%%%%          ##### ###  ######### ####### ######
-//  %%%%%%%%%%          #### ##               ####### ####
-//  %🌸%%%%%%%%%           ## #                  ##### ###
-//   %%  %% % %%         # ##                      ## ###
-//     %   %    %        # ###                      # ###
-//                        # ###                     ## ###
-//                        # ###                     ## ###
-//                        # ####                   #### ##
-//                       ### ###                  ##### ###
-//                      ####  ###                 ####   ##
-//                     #####   ###                 ##    ##
-//                    #####    ####                      ###
-//                     ##        ###                     ###
-//                                ####                     ##
-//                                 ####                    ###
-//                                                         ####
-//                                                          ##
 //
-// This is a template for the final project. You'll be setting up a stable and making
-// sure it runs smoothly.
-//
-// You can copy and paste this into your stables.js file, and then use it to guide you.
-// Please do not create a new file!
-// Feel free to keep the headings (e.g.: "Variables"), but please delete any boilerplate
-// comments. (Your own comments, where necessary, are acceptable.)
-//
-// NOTE: UNLESS SPECIFIED, VARIABLE NAMING IS UP TO YOU. THERE SHOULD BE NO HARD-CODED
-// NUMBERS OR STRINGS WHERE VARIBALES WOULD BE PREFERRED.
 
-//------------------------- Seting up shop -------------------------//
+//------------------------- Setting up shop -------------------------//
 
-// Initialize a variable called "horses" that will store the horses in your stable.
-// Create a variable and use it to store a message visitors to the stable.
-// Create a variable and use it to store the cost of a late payment fee.
-// Create a variable and use it to store the number of available stalls in the stable.
+let horses = [];
+let welcomeMsg = "Welcome to Star Rail Stables!";
+let latePenalty = 0.1;
+let stallsAvail = 8;
 
 //------------------------- First day -------------------------//
 
-// Using a constructor, add at least 3 horses to your stables.
-//
-// The horse should have the following properties:
-// - name, nickname, favorite treat (string)
-// - age, monthly rent (number)
-// - location (boolean inside/outside)
-// - two unique properties of your choice (use any primitive)
-// - a method that introduces your horse and references at least 3 object properties.
-// - a second, unique method of your choice
+function Horse(
+    name,
+    nickname,
+    favTreat,
+    age,
+    rent,
+    isInside,
+    coatColour,
+    personality
+) {
+    this.name = name;
+    this.nickname = nickname;
+    this.favTreat = favTreat;
+    this.age = age;
+    this.rent = rent;
+    this.isInside = isInside;
+    this.coatColour = coatColour;
+    this.personality = personality;
+    this.introduction = function () {
+        console.log(
+            `This is ${this.nickname}. They are ${this.age} years old and they are a ${this.personality} horse.`
+        );
+    };
+    this.brushCoat = function () {
+        console.log(
+            `${this.nickname} is just brushed, their ${this.coatColour} coat is all shiny and beautiful.`
+        );
+    };
+}
 
-// Store the horses you've just created in your "horses" variable.
+let ratio = new Horse(
+    "Veritas",
+    "Dr.Ratio",
+    "blueberries",
+    8,
+    800,
+    true,
+    "navy",
+    "stoic"
+);
 
-// Fancy! Another horse wants to be stabled! Create a variable that stores an object
-// literal of your new horse, and add it to your "horses" variable.
+let aventurine = new Horse(
+    "Kakavasha",
+    "Aventurine",
+    "chips",
+    5,
+    777,
+    true,
+    "golden",
+    "clever"
+);
 
-// Initialize new property to your horses: a boolean that allows you to check if your horse
-// is hungry or not. Use dot notation.
+let topaz = new Horse(
+    "Jelena",
+    "Topaz",
+    "cake",
+    7,
+    600,
+    false,
+    "maroon",
+    "cheerful"
+);
+
+horses.push(ratio);
+horses.push(aventurine);
+horses.push(topaz);
+
+let jade = {
+    name: "Bonajade",
+    nickname: "Jade",
+    favTreat: "cherries",
+    age: 11,
+    rent: 1200,
+    isInside: false,
+    coatColour: "violet",
+    personality: "cynical",
+    introduction() {
+        console.log(
+            `This is ${this.nickname}. They are ${this.age} years old and they are a ${this.personality} horse.`
+        );
+    },
+    brushCoat() {
+        console.log(
+            `${this.nickname} is just brushed, their ${this.coatColour} coat is all shiny and beautiful.`
+        );
+    },
+};
+
+horses.push(jade);
+
+for (let i = 0; i < horses.length; i++) {
+    horses[i].isHungry = true;
+}
 
 //------------------------- Stable roster -------------------------//
 
-// We want to keep track of our horses and how they are doing. Paste the data structure
-// of our horse variable below so we can reference it. It is okay to comment it out.
+// [
+//     Horse {
+//       name: 'Veritas',
+//       nickname: 'Dr.Ratio',
+//       favTreat: 'blueberries',
+//       age: 8,
+//       rent: 800,
+//       isInside: true,
+//       coatColour: 'navy',
+//       personality: 'stoic',
+//       introduction: [Function (anonymous)],
+//       brushCoat: [Function (anonymous)],
+//       isHungry: true
+//     },
+//     Horse {
+//       name: 'Kakavasha',
+//       nickname: 'Aventurine',
+//       favTreat: 'chips',
+//       age: 5,
+//       rent: 777,
+//       isInside: true,
+//       coatColour: 'golden',
+//       personality: 'clever',
+//       introduction: [Function (anonymous)],
+//       brushCoat: [Function (anonymous)],
+//       isHungry: true
+//     },
+//     Horse {
+//       name: 'Jelena',
+//       nickname: 'Topaz',
+//       favTreat: 'cake',
+//       age: 7,
+//       rent: 600,
+//       isInside: false,
+//       coatColour: 'maroon',
+//       personality: 'cheerful',
+//       introduction: [Function (anonymous)],
+//       brushCoat: [Function (anonymous)],
+//       isHungry: true
+//     },
+//     {
+//       name: 'Bonajade',
+//       nickname: 'Jade',
+//       favTreat: 'cherries',
+//       age: 11,
+//       rent: 1200,
+//       isInside: false,
+//       coatColour: 'violet',
+//       personality: 'cynical',
+//       introduction: [Function: introduction],
+//       brushCoat: [Function: brushCoat],
+//       isHungry: true
+//     }
+//   ]
 
 //---------------------- Growing business ----------------------//
 
-// We've added some horses! Re-assign the variable that contains the number of available stalls.
-// Use the math operator to determine how many stables you should have left, given the number of
-// horses you've added.
+stallsAvail = stallsAvail - horses.length;
 
-// Express the following in code: If the variable that contains your remaining stalls
-// is less than 2, log out "We need to build more stalls", otherwise log out "We have
-// [number of stalls] available!"
+if (stallsAvail < 2) {
+    console.log("Quick, we need to build more stalls!");
+} else {
+    console.log(`We have ${stallsAvail} stall(s) available! 😎`);
+}
 
-// Create a function that logs out how much will an individual horse will owe if rent is
-// paid late. Invoke the function.
+function lateFeeOwed(horse) {
+    let lateFee = horse.rent * latePenalty;
+    console.log(
+        `${horse.nickname} owes an extra $${lateFee} if rent is paid late.`
+    );
+}
+lateFeeOwed(horses[0]);
 
-// Add loop that checks to find out if a certain horse likes a treat. If the horse does not like
-// the treat, log out their disatisfaction and check the next horse. Exit the loop when you find
-// the horse that likes the treat.
+function checkFavTreat(treat) {
+    for (let i = 0; i < horses.length; i++) {
+        if (horses[i].favTreat === treat) {
+            return console.log(
+                `${horses[i].nickname} loves ${treat} so much they gobbled it all up! 😋`
+            );
+        }
 
-// Create and invoke a function that returns (not logs) the nickname of a chosen horse. Log out
-// the return value outside of the function.
+        console.log(
+            `${horses[i].nickname} doesn't like ${treat}...maybe try something else.`
+        );
+    }
+}
+checkFavTreat("cake");
+
+function getNickname(horse) {
+    return horse.nickname;
+}
+console.log(`${horses[1].name}'s nickname is ${getNickname(horses[1])}.`);
 
 //------------------------- Day to day operations -------------------------//
 
-// Create and invoke a function references one of your unique horse properties. It should
-// contain a conditional or a loop.
+function petHorse(horse) {
+    if (horse.personality === "cynical") {
+        console.log(
+            `${horse.nickname} is wary of you, they trotted away quickly.`
+        );
+    } else if (horse.personality === "clever") {
+        console.log(
+            `${horse.nickname} will only let you pet them if you give them a treat.`
+        );
+    } else {
+        console.log(`${horse.nickname} lowered their head for you to pet. 🫳`);
+    }
+}
+petHorse(horses[2]);
+petHorse(horses[3]);
 
-// Morning! Create and invoke a function that moves all your horses outside. It should
-// log out statement indicating the horses have been moved to spend time in the sun.
+for (let i = 0; i < horses.length; i++) {
+    horses[i].switchLocation = function () {
+        if (this.isInside) {
+            this.isInside = false;
+        } else {
+            this.isInside = true;
+        }
+    };
+}
 
-// Initialize a method on your horses that moves them inside if they are outside, and
-// vice versa.
+function feedHorses() {
+    for (let i = 0; i < horses.length; i++) {
+        if (!horses[i].isInside) {
+            horses[i].switchLocation();
+            console.log(`Calling ${horses[i].nickname} inside for a treat...`);
+        }
 
-// Food! Create and invoke a function that feeds your horses. If the horse is outside, it
-// should call them in, and then all horses should be fed a treat. Log the activity of
-// the horses.
+        if (horses[i].isHungry) {
+            horses[i].isHungry = false;
+            console.log(`Feeding treats to ${horses[i].nickname}...`);
+        }
+    }
 
-// Bedtime! Update the function that lets your horses outside so that it also calls them
-// in if it is getting dark. Only the horses that moved should log out a message. Invoke the function.
+    console.log("All horses have been fed. 🍽️");
+}
+feedHorses();
 
-//------------------------- Bonus -------------------------//
+function moveOutside() {
+    for (let i = 0; i < horses.length; i++) {
+        if (horses[i].isInside) {
+            horses[i].isInside = false;
+        }
+    }
+    console.log("Good morning! Bringing all horses out to play. 🌤️");
 
-// Replace the ascii art at the top of the file with art representing your stable.
+    console.log("It's getting dark, calling horses inside...");
+
+    for (let i = 0; i < horses.length; i++) {
+        if (!horses[i].isInside) {
+            horses[i].isInside = true;
+        }
+    }
+    console.log("All horses are now inside. Good night! 💤");
+}
+moveOutside();
